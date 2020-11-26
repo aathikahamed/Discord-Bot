95c2655417737676b3fac1345ae70a0fe639774b
Aathik Ahamed <aathikahamed@gmail.com>
2020-11-26 21:05:40 +0000

    Making a fetch request

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ client.login(process.env.DISCORD_BOT_TOKEN);
 client.on("message", (msg) => {
   if (msg.channel.id === "781604557985284196" && msg.content[0] === "!") {
     axios.get("https://disease.sh/v3/covid-19/all").then((response) => {
-      msg.channel.send("Cases " + response);
+      msg.channel.send("Cases " + response.cases);
     });
   }
 });
