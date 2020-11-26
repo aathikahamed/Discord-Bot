d7d8ab3329ff37376369292c705d454d1523a180
Aathik Ahamed <aathikahamed@gmail.com>
2020-11-26 21:03:48 +0000

    Changed channel ID

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ require("dotenv").config();
 client.login(process.env.DISCORD_BOT_TOKEN);
 
 client.on("message", (msg) => {
-  if (msg.channel.id === "781600324917788723" && msg.content[0] === "!") {
+  if (msg.channel.id === "781604557985284196" && msg.content[0] === "!") {
     axios.get("https://disease.sh/v3/covid-19/all").then((response) => {
       msg.channel.send("Cases" + response.cases);
     });
