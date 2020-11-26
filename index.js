5fbf9b1780213462c9f8103d21fbce264ee889f8
Aathik Ahamed <aathikahamed@gmail.com>
2020-11-26 20:32:27 +0000

    Added start script

--- index.js
+++ index.js
@@ -4,10 +4,6 @@ require("dotenv").config();
 
 client.login(process.env.DISCORD_BOT_TOKEN);
 
-client.on("ready", () => {
-  console.log(`Logged in as ${client.user.tag}!`);
-});
-
 client.on("message", (msg) => {
   if (msg.channel.id === "781600324917788723" && msg.content === "ping") {
     msg.channel.send("😎😂🤯");
