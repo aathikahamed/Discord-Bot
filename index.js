4956fc8b48ce50884c1fec1f0ee1c2869803d5da
Aathik Ahamed <aathikahamed@gmail.com>
2020-11-26 21:14:18 +0000

    Added embed message

--- index.js
+++ index.js
@@ -1,15 +1,36 @@
 const axios = require("axios");
 const Discord = require("discord.js");
 const client = new Discord.Client();
-
 require("dotenv").config();
+
+const exampleEmbed = new Discord.MessageEmbed()
+  .setColor("#0099ff")
+  .setTitle("Some title")
+  .setURL("https://discord.js.org/")
+  .setAuthor(
+    "Some name",
+    "https://i.imgur.com/wSTFkRM.png",
+    "https://discord.js.org"
+  )
+  .setDescription("Some description here")
+  .setThumbnail("https://i.imgur.com/wSTFkRM.png")
+  .addFields(
+    { name: "Regular field title", value: "Some value here" },
+    { name: "\u200B", value: "\u200B" },
+    { name: "Inline field title", value: "Some value here", inline: true },
+    { name: "Inline field title", value: "Some value here", inline: true }
+  )
+  .addField("Inline field title", "Some value here", true)
+  .setImage("https://i.imgur.com/wSTFkRM.png")
+  .setTimestamp()
+  .setFooter("Some footer text here", "https://i.imgur.com/wSTFkRM.png");
 
 client.login(process.env.DISCORD_BOT_TOKEN);
 
 client.on("message", (msg) => {
   if (msg.channel.id === "781604557985284196" && msg.content[0] === "!") {
     axios.get("https://disease.sh/v3/covid-19/all").then((response) => {
-      msg.channel.send("Cases " + response.data.cases);
+      msg.channel.send(exampleEmbed);
     });
   }
 });
