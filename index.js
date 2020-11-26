62bf96995a797e7c07cc4ec4007c6d0bae076e4b
Aathik Ahamed <aathikahamed@gmail.com>
2020-11-26 20:56:27 +0000

    Making a fetch request

--- index.js
+++ index.js
@@ -1,11 +1,15 @@
+const axios = require("axios");
 const Discord = require("discord.js");
 const client = new Discord.Client();
+
 require("dotenv").config();
 
 client.login(process.env.DISCORD_BOT_TOKEN);
 
 client.on("message", (msg) => {
-  if (msg.channel.id === "781600324917788723" && msg.content === "ping") {
-    msg.channel.send("😎😂🤯");
+  if (msg.channel.id === "781600324917788723" && msg.content[0] === "!") {
+    axios.get("https://disease.sh/v3/covid-19/all").then((response) => {
+      msg.channel.send(response);
+    });
   }
 });
